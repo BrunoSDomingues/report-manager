8b8bb66aec8737e837d88a54b98d000def178491
Bruno Domingues <brunao.signorelli@hotmail.com>
2023-06-13 18:54:53 -0300

    New routes

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from starlette.responses import RedirectResponse
-import random
+from typing import Optional
+from pydantic import BaseModel
+
+
+class Operation(BaseModel):
+    quantity: int
+    op_type: str
+    value: Optional[float] = None
+
+
+class Report(BaseModel):
+    new_clients: int
+    new_ops: Operation
+    paid_ops: Operation
+    pipeline: Operation
+    stock: Operation
+
 
 app = FastAPI()
 
 
-@app.get("/rand")
-async def rand():
-    return random.randint(0, 100)
+@app.post("/report/")
+async def create_report(report: Report):
+    return report
+
+
+@app.get("/reports/{report_id}")
+async def read_item(report_id: int):
+    return {"report_id": report_id}
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
